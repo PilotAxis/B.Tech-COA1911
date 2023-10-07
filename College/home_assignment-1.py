1abb9e49074f5d7e42222eee2d6f026d28313a73
Mohammed Ahmed Majid <109688855+PilotAxis@users.noreply.github.com>
2023-10-07 18:50:25 +0530

    Python File

---
+++ College/home_assignment-1.py
@@ -0,0 +1,15 @@
+# a.
+print("Mohammed Ahmed Majid")
+print("Petrochemical Engineering")
+
+# b.
+a = 2
+b = 3
+Sum = a+b
+print("The sum is :", Sum)
+
+# c.
+print(1 + 2 * 3 / 4)
+print(1 + ( 2 * 3 ) / 4)
+print(( 1 + 2 ) * 3 / 4)
+print(( ( 1 + 2 ) * 3 ) / 4)
