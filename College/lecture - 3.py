5aba1765b60901efd6c5caec3f6b47f8f65e244d
Mohammed Ahmed Majid <109688855+PilotAxis@users.noreply.github.com>
2023-10-13 11:38:16 +0530

    New code uploaded

---
+++ College/lecture - 3.py
@@ -0,0 +1,24 @@
+#1.
+'''avg = 0
+a = int(input("Enter a number :"))
+b = int(input("Enter a number :"))
+c = int(input("Enter a number :"))
+avg = (a + b + c)/3
+print("Averege of these numbers is :", avg)
+'''
+#2.
+'''sub1 = float(input("Enter marks :"))
+sub2 = float(input("Enter marks :"))
+sub3 = float(input("Enter marks :"))
+sub4 = float(input("Enter marks :"))
+sub5 = float(input("Enter marks :"))
+tot = sub1+sub2+sub3+sub4+sub5
+per = (tot*100)/500
+print(per)'''
+
+#3.
+a = int(input("Enter a number :"))
+b = int(input("Enter a number :"))
+print("Before swapping :", a, b)
+a,b=b,a
+print("After swapping :", a, b)
