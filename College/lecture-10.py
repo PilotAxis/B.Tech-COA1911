7b9e634042628dc056b8c22602a6f7421c1653b3
PilotAxis <mohammedahmedmajid14@gmail.com>
2023-11-07 11:20:32 +0530

    new files

---
+++ College/lecture-10.py
@@ -0,0 +1,16 @@
+#1.
+'''
+myfile = open(r"/Users/ahmedmajid/My Drive/File Handling Files/poem.txt", "r")
+#con = myfile.read()
+con1 = myfile.readline()
+#con2 = myfile.readlines()
+
+
+#print(con)
+#print()
+print(con1)
+#print()
+#print(con2)
+
+myfile.close()
+'''
